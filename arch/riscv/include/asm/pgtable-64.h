a5aacb569b0a61f3d893320cd2e64ddf03372ef4
kanghayoung76 <rkdgkdud@pusan.ac.kr>
2025-08-24 17:04:27 +0900

    arch/riscv/include/asm/pgtable-64.h

--- arch/riscv/include/asm/pgtable-64.h
+++ arch/riscv/include/asm/pgtable-64.h
@@ -9,6 +9,7 @@
 #include <linux/bits.h>
 #include <linux/const.h>
 #include <asm/alternative-macros.h>
+#include <asm/genesis.h>
 
 extern bool pgtable_l4_enabled;
 extern bool pgtable_l5_enabled;
@@ -212,7 +213,13 @@ static inline int pud_user(pud_t pud)
 
 static inline void set_pud(pud_t *pudp, pud_t pud)
 {
+#ifndef CONFIG_GENESIS
 	WRITE_ONCE(*pudp, pud);
+#else
+        _genesis_entry(/*svc_num*/ GENESIS_SET_PUD,
+                       /*arg0*/ (unsigned long)pudp,
+                       /*arg1*/ pud_val(pud));
+#endif
 }
 
 static inline void pud_clear(pud_t *pudp)
@@ -288,7 +295,13 @@ static inline pmd_t pfn_pmd(unsigned long pfn, pgprot_t prot)
 static inline void set_p4d(p4d_t *p4dp, p4d_t p4d)
 {
 	if (pgtable_l4_enabled)
+#ifndef CONFIG_GENESIS
 		WRITE_ONCE(*p4dp, p4d);
+#else
+                _genesis_entry(/*svc_num*/ GENESIS_SET_P4D,
+                               /*arg0*/ (unsigned long)p4dp,
+                               /*arg1*/ p4d_val(p4d));
+#endif
 	else
 		set_pud((pud_t *)p4dp, (pud_t){ p4d_val(p4d) });
 }
@@ -356,6 +369,17 @@ static inline struct page *p4d_page(p4d_t p4d)
 #define pud_offset pud_offset
 pud_t *pud_offset(p4d_t *p4d, unsigned long address);
 
+#ifdef CONFIG_GENESIS
+static inline void set_pgd(pgd_t *pgdp, pgd_t pgd)
+{
+        if (pgtable_l5_enabled)
+                _genesis_entry(/*svc_num*/ GENESIS_SET_PGD,
+                               /*arg0*/ (unsigned long)pgdp,
+                               /*arg1*/ pgd_val(pgd));
+        else
+                set_p4d((p4d_t *)pgdp, (p4d_t){ pgd_val(pgd) });
+}
+#else
 static inline void set_pgd(pgd_t *pgdp, pgd_t pgd)
 {
 	if (pgtable_l5_enabled)
@@ -363,6 +387,7 @@ static inline void set_pgd(pgd_t *pgdp, pgd_t pgd)
 	else
 		set_p4d((p4d_t *)pgdp, (p4d_t){ pgd_val(pgd) });
 }
+#endif
 
 static inline int pgd_none(pgd_t pgd)
 {
